4a8d3db4e02d854fd6fd21e43b77814e42c51421
mindemory <mrugankdake@gmail.com>
2022-03-17 14:34:50 -0400

    Subject 03 session 01: trial Phosphene mapping success with phosphenes reported in the upper right quadrant.

--- tmsRoom/Retinitopy/recordPhosphene.m
+++ tmsRoom/Retinitopy/recordPhosphene.m
@@ -32,7 +32,7 @@
 clear; close all; clc;% clear mex;
 global parameters screen hostname kbx mbx
 
-subjID = '100';
+subjID = '03';
 session = '01';
 
 % Check the system name to ensure correct paths are added.
