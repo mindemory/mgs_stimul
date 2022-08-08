0058f5b7294024acda6ab2e2fc4e4dc7aab91ed9
mindemory <mrugankdake@gmail.com>
2022-08-08 14:13:39 -0400

    minor changes

--- mgsTask/mgs_practice.m
+++ mgsTask/mgs_practice.m
@@ -37,7 +37,7 @@ elseif strcmp(hostname, 'tmsubuntu')
     addpath(genpath('/usr/share/psychtoolbox-3'))
     parameters.isDemoMode = false; %set to true if you want the screen to be transparent
     parameters.EEG = 0; % set to 0 if there is no EEG recording
-    parameters.TMS = 1; % set to 0 if there is no TMS stimulation
+    parameters.TMS = 0; % set to 0 if there is no TMS stimulation
     parameters.eyetracker = 1; % set to 0 if there is no eyetracker
     PsychDefaultSetup(1);
 else
