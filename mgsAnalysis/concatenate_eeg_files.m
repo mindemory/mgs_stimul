d599163d2e7c8690a2afcf2ff877fab7804f7c45
mindemory <mrugankdake@gmail.com>
2022-09-04 11:48:52 -0400

    minor changes

--- mgsAnalysis/concatenate_eeg_files.m
+++ mgsAnalysis/concatenate_eeg_files.m
@@ -21,27 +21,25 @@ end
 %    'sub01_day01_block10.vhdr'};
 concatfname = ['sub' num2str(subjID, "%02d") '_day' num2str(day, "%02d") '_concat.vhdr'];
 concatfilepathname = [EEGpath filesep concatfname];
-%if ~exist(concatfilename)
-    data_concat = NaN(63,1);
-    sample_count = 0;
-    for ii=1:length(EEGfiles)
-        EEGfile = [EEGpath filesep EEGfiles{ii}];
-        disp(['Working on file ' EEGfiles{ii}]);
-        hdr = ft_read_header(EEGfile);
-        dat = ft_read_data(EEGfile);
-        evt = ft_read_event(EEGfile);
-        if ii == 1
-            event_concat = evt;
-        else
-            for jj=1:length(evt)
-                evt(jj).sample = evt(jj).sample + sample_count;
-            end
-            event_concat = [event_concat, evt];
+data_concat = NaN(63,1);
+sample_count = 0;
+for ii=1:length(EEGfiles)
+    EEGfile = [EEGpath filesep EEGfiles{ii}];
+    disp(['Working on file ' EEGfiles{ii}]);
+    hdr = ft_read_header(EEGfile);
+    dat = ft_read_data(EEGfile);
+    evt = ft_read_event(EEGfile);
+    if ii == 1
+        event_concat = evt;
+    else
+        for jj=1:length(evt)
+            evt(jj).sample = evt(jj).sample + sample_count;
         end
-        sample_count = sample_count + hdr.nSamples;
-        data_concat = [data_concat, dat(1:63, :)];
+        event_concat = [event_concat, evt];
     end
-    hdr.nSamples = sample_count;
-    hdr.nChans = 63;
-    ft_write_data(concatfilepathname, data_concat, 'header', hdr, 'event', event_concat);
-%end
+    sample_count = sample_count + hdr.nSamples;
+    data_concat = [data_concat, dat(1:63, :)];
+end
+hdr.nSamples = sample_count;
+hdr.nChans = 63;
+ft_write_data(concatfilepathname, data_concat, 'header', hdr, 'event', event_concat);
