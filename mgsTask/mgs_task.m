e52a0be6ab2469e4e9dc676f722e5f66684c1a63
mindemory <mrugankdake@gmail.com>
2022-11-08 09:53:22 -0500

    tms on

--- mgsTask/mgs_task.m
+++ mgsTask/mgs_task.m
@@ -58,7 +58,7 @@ elseif strcmp(hostname, 'tmsubuntu') % Running stimulus code for testing
         end_block = 2; % 2 blocks for practice session
         mgs_data_path = [master_dir '/data/mgs_practice_data/sub' subjID];
     else
-        parameters.EEG = 0;%1; % set to 0 if there is no EEG recording
+        parameters.EEG = 1; % set to 0 if there is no EEG recording
         end_block = 10; % 10 blocks for main sessions
         mgs_data_path = [master_dir '/data/mgs_data/sub' subjID];
     end
@@ -125,9 +125,9 @@ for block = start_block:end_block
         datapath = [mgs_data_path '/day' num2str(day, "%02d")];
         parameters = initFiles(parameters, screen, datapath, kbx, block);
         if taskMap(1).TMScond == 1 % determine if this is a TMS task
-            parameters.TMS = 0;%1;
+            parameters.TMS = 1;
         elseif taskMap(1).TMScond == 0
-            parameters.TMS = 0;
+            parameters.TMS = 1;
         end
         tMap = taskMap(1, block);
     end
