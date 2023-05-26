24fa3b0b0a4c20190e5522dedbc9e7aa9e13c0de
mindemory <mrugankdake@gmail.com>
2023-05-26 15:37:50 -0400

    analysis in progress

--- mgsAnalysis/analysisEye/helpers.py
+++ mgsAnalysis/analysisEye/helpers.py
@@ -3,7 +3,12 @@ import pandas as pd
 
 def rotate_to_zero(df):
     TarTheta = np.arctan2(df['TarY'], df['TarX'])
-    TarRadius = np.sqrt(df['TarX']^2+df['TarY']^2)
+    df['TarTheta'] = TarTheta
+    df['TarRadius'] = np.sqrt(df['TarX']**2+df['TarY']**2)
+    df['isaccTheta'] = np.arctan2(df['isaccY'], df['isaccX'])
+    df['isaccRadius'] = np.sqrt(df['isaccX']**2+df['isaccY']**2)
+    df['fsaccTheta'] = np.arctan2(df['fsaccY'], df['fsaccX'])
+    df['fsaccRadius'] = np.sqrt(df['fsaccX']**2+df['fsaccY']**2)
     # Initialize columns to store rotated anlges
     df['TarX_rotated'] = np.zeros(len(df['TarX']))
     df['TarY_rotated'] = np.zeros(len(df['TarX']))
@@ -14,16 +19,20 @@ def rotate_to_zero(df):
 
     xcols = ['TarX', 'isaccX', 'fsaccX']
     ycols = ['TarY', 'isaccY', 'fsaccY']
+    radiuscols = ['TarRadius', 'isaccRadius', 'fsaccRadius']
+    angluarcols = ['TarTheta', 'isaccTheta', 'fsaccTheta']
     for ii in range(len(TarTheta)):
         # Get angle for target and corresponding rotation matrix
         this_angle = -1 * TarTheta[ii]
-        radial_error = np.max(TarRadius) - TarRadius[ii]
+        radial_error = np.max(df['TarRadius']) - df.loc[ii, ['TarRadius']][0]
         rotation_matrix = np.array([[np.cos(this_angle), -np.sin(this_angle)],
                                     [np.sin(this_angle), np.cos(this_angle)]])
         for idx in range(len(xcols)):
-            x = df.loc[ii, [xcols[idx]]]
-            y = df.loc[ii, [ycols[idx]]]
+            
+            x = df.loc[ii, [xcols[idx]]] + radial_error * np.cos(df.loc[ii, [angluarcols[idx]]][0])
+            y = df.loc[ii, [ycols[idx]]] + radial_error * np.sin(df.loc[ii, [angluarcols[idx]]][0])
             input_points = np.vstack((x, y))
+            #print(input_points)
             rotated_points = np.dot(rotation_matrix, input_points)
             df.loc[ii, [xcols[idx] + '_rotated']] = rotated_points[0]
             df.loc[ii, [ycols[idx] + '_rotated']] = rotated_points[1]
