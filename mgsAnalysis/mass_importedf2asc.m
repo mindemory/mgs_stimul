cef50b84c1bfcad0aa743641a52bcd33b6ef528f
mindemory <mrugankdake@gmail.com>
2023-07-19 14:56:42 -0400

    minor edits to eeg pipeline

--- mgsAnalysis/mass_importedf2asc.m
+++ mgsAnalysis/mass_importedf2asc.m
@@ -3,10 +3,10 @@ subs = [1, 3, 6, 8, 12, 13, 14, 15, 16];
 days = [1, 2, 3];
 parfor ss = 1:length(subs)
     this_sub = subs(ss);
-    %A01_GetTimeReport(this_sub);
-    for dd = days
-        A02_EyeAnalysis(this_sub, dd);
-    end
+    A01_GetTimeReport(this_sub);
+%     for dd = days
+%         A02_EyeAnalysis(this_sub, dd);
+%     end
 end
 
 subs = [ 3, 6, 8, 12, 13, 14, 15, 16];
