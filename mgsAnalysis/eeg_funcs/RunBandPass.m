6927373b77f11fc5df83bf548fb2f97ca7f9dd73
mindemory <mrugankdake@gmail.com>
2024-01-12 17:06:18 -0500

    minor update to EEG pipeline

--- mgsAnalysis/eeg_funcs/RunBandPass.m
+++ mgsAnalysis/eeg_funcs/RunBandPass.m
@@ -2,8 +2,8 @@ function data_eeg           = RunBandPass(data_eeg)
 % Created by Mrugank (10/23/2023): Applies
 cfg                         = [];
 cfg.bpfilter                = 'yes';
+cfg.bpfilttype              = 'firws';
+cfg.bpfiltdir               = 'onepass-zerophase';
 cfg.bpfreq                  = [0.5 50];
-% cfg.dftfilter               = 'yes';
-% cfg.dftfreq                 = 60:60:420;
 data_eeg                    = ft_preprocessing(cfg, data_eeg);
 end
