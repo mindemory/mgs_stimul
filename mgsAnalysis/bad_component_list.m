fd52aa4c074903fcfb4ab2a12a4efe800d0d034f
Mrugank Dake <mrugankdake@gmail.com>
2024-01-16 11:22:52 -0500

    minor

---
+++ mgsAnalysis/bad_component_list.m
@@ -0,0 +1,9 @@
+function bad_comps = bad_component_list(subjID, day)
+if subjID == 1
+    if day == 1
+        bad_comps = [27 35];
+    elseif day == 2
+        bad_comps = [30 39];
+    end
+
+end
