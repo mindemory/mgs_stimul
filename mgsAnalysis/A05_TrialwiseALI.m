bcd2cf44777657526d54e83e889e98e6325b3a7e
Mrugank Dake <mrugankdake@gmail.com>
2024-01-25 10:06:26 -0500

    some reorganizing

--- mgsAnalysis/A05_TrialwiseALI.m
+++ mgsAnalysis/A05_TrialwiseALI.m
@@ -83,7 +83,6 @@ if ~exist(fName.mALI, 'file')
                 this_contra                 = combineTFRs_ALI(POW.(ttype), this_hemi, 0, fidx);
                 cfg                         = [];
                 cfg.operation               = '(10^(x1/10) - 10^(x2/10)) / (10^(x1/10) + 10^(x2/10))';
-%                 cfg.operation               = '(x1-x2)';
                 cfg.parameter               = 'powspctrm';
                 pow_diff                    = ft_math(cfg, this_contra, this_ipsi);
                 tcount                      = size(pow_diff.powspctrm, 1);
@@ -102,91 +101,6 @@ if ~exist(fName.mALI, 'file')
                 temp_trl_mat(:, 5)          = setdiff(trl_idx.(ttype), trls_to_remove);
                 trl_mat                     = [trl_mat; temp_trl_mat];
             end
-            
-            % Combine TFRs for this subject over all days based on trial and
-            % session type
-%             if p.day == NoTMSDays(subjID)
-%                 disp(['Combining TFRs No TMS, day = ' num2str(p.day)])
-%                 [TFR.NT.pin.ipsi, TFR.NT.pin.contra] ...
-%                     = combineTFR_notms_ALI(POW, this_hemi, 'pin', 'pout');
-%                 
-%                 [TFR.NT.ain.ipsi, TFR.NT.ain.contra] ...
-%                     = combineTFR_notms_ALI(POW, this_hemi, 'ain', 'aout');
-%                 
-%             else
-%                 disp(['Combining TFRs TMS, day = ' num2str(p.day)])
-%                 if ~exist('TFR', 'var') || ~isfield(TFR, 'T')
-%                     
-% 
-%                     TFR.T.pin.ipsi          = combineTFRs_ALI(POW.pin, this_hemi, 1);
-%                     TFR.T.pin.contra        = combineTFRs_ALI(POW.pin, this_hemi, 0);
-%                     TFR.T.pout.ipsi         = combineTFRs_ALI(POW.pout, this_hemi, 1);
-%                     TFR.T.pout.contra       = combineTFRs_ALI(POW.pout, this_hemi, 0);
-%     
-%                     TFR.T.ain.ipsi          = combineTFRs_ALI(POW.ain, this_hemi, 1);
-%                     TFR.T.ain.contra        = combineTFRs_ALI(POW.ain, this_hemi, 0);
-%                     TFR.T.aout.ipsi         = combineTFRs_ALI(POW.aout, this_hemi, 1);
-%                     TFR.T.aout.contra       = combineTFRs_ALI(POW.aout, this_hemi, 0);
-%                 else
-%                     TFR.T.pin.ipsi          = combineTFRs_ALI(POW.pin, this_hemi, 1, TFR.T.pin.ipsi);
-%                     TFR.T.pin.contra        = combineTFRs_ALI(POW.pin, this_hemi, 0, TFR.T.pin.contra);
-%                     TFR.T.pout.ipsi         = combineTFRs_ALI(POW.pout, this_hemi, 1, TFR.T.pout.ipsi);
-%                     TFR.T.pout.contra       = combineTFRs_ALI(POW.pout, this_hemi, 0, TFR.T.pout.contra);
-%     
-%                     TFR.T.ain.ipsi          = combineTFRs_ALI(POW.ain, this_hemi, 1, TFR.T.ain.ipsi);
-%                     TFR.T.ain.contra        = combineTFRs_ALI(POW.ain, this_hemi, 0, TFR.T.ain.contra);
-%                     TFR.T.aout.ipsi         = combineTFRs_ALI(POW.aout, this_hemi, 1, TFR.T.aout.ipsi);
-%                     TFR.T.aout.contra       = combineTFRs_ALI(POW.aout, this_hemi, 0, TFR.T.aout.contra);
-%                 end
-%             end
-%         end
-%     
-%         
-%         % Store for combined analysis
-%         for cond = conds
-%             for ttype = t_types
-%                 tcount = size(TFR.(cond).(ttype).powspctrm, 1);
-%                 temp_trl_mat = NaN(tcount, 4);
-%                 temp_trl_mat(:, 1) = subjID;
-%                 temp_trl_mat(:, 2) = strcmp(cond, 'T');
-%                 temp_trl_mat(:, 3) = ttype;
-%                 temp_trl_mat(:, 4) = setdiff(trl_idx.(cond), trls_to_remove);
-%                 mALI = cat(1, mALI, TFR.(cond).(ttype).powspctrm);
-% 
-%             end
-%         end
-% 
-%         if ~isfield(mALI, 'NT')
-%             for tt                          = t_types_in
-%                 mALI.NT.(tt).ipsi           = TFR.NT.(tt).ipsi;
-%                 mALI.NT.(tt).contra         = TFR.NT.(tt).contra;
-%                 mALI.NT.(tt).all            = TFR.NT.(tt).all;
-%             end
-%             mALI.NT.pout.all                = TFR.NT.pout.all;
-%             mALI.NT.aout.all                = TFR.NT.aout.all;
-%         else
-%             for tt                          = t_types_in
-%                 mALI.NT.(tt).ipsi           = subject_level_grouping(mALI.NT.(tt).ipsi, TFR.NT.(tt).ipsi);
-%                 mALI.NT.(tt).contra         = subject_level_grouping(mALI.NT.(tt).contra, TFR.NT.(tt).contra);
-%                 mALI.NT.(tt).all            = subject_level_grouping(mALI.NT.(tt).all, TFR.NT.(tt).all, 1);
-%             end
-%             mALI.NT.pout.all                = subject_level_grouping(mALI.NT.pout.all, TFR.NT.pout.all, 1);
-%             mALI.NT.aout.all                = subject_level_grouping(mALI.NT.aout.all, TFR.NT.aout.all, 1);
-%         end
-%     
-%         if ~isfield(mALI, 'T')
-%             for tt = t_types
-%                 mALI.T.(tt).ipsi            = TFR.T.(tt).ipsi;
-%                 mALI.T.(tt).contra          = TFR.T.(tt).contra;
-%                 mALI.T.(tt).all             = TFR.T.(tt).all;
-%             end
-%         else
-%             for tt = t_types
-%                 mALI.T.(tt).ipsi            = subject_level_grouping(mALI.T.(tt).ipsi, TFR.T.(tt).ipsi);
-%                 mALI.T.(tt).contra          = subject_level_grouping(mALI.T.(tt).contra, TFR.T.(tt).contra);
-%                 mALI.T.(tt).all             = subject_level_grouping(mALI.T.(tt).all, TFR.T.(tt).all, 1);
-%             end
-%         end
         end
         clearvars POW;
     end
